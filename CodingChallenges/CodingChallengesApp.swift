d42bbfb183eaf2ab3078b1275d688c7c69822d7e
brewdiHQ <stephan.sattel@icloud.com>
2024-09-09 10:05:19 +0200

    Initial Commit

---
+++ CodingChallenges/CodingChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingChallengesApp.swift
+//  CodingChallenges
+//
+//  Created by Cokohapa on 09.09.24.
+//
+
+import SwiftUI
+
+@main
+struct CodingChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
