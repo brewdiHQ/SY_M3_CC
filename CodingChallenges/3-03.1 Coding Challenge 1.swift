35a60e8898ab1f64d025f82be27d23edc494cc5e
brewdiHQ <stephan.sattel@icloud.com>
2024-09-09 10:08:25 +0200

    add list

---
+++ CodingChallenges/3-03.1 Coding Challenge 1.swift
@@ -0,0 +1,18 @@
+//
+//  3-03.1 Coding Challenge 1.swift
+//  CodingChallenges
+//
+//  Created by Cokohapa on 09.09.24.
+//
+
+import SwiftUI
+
+struct __03_1_Coding_Challenge_1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    __03_1_Coding_Challenge_1()
+}
